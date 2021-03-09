3753a2b48786a6ac0b8d343eb96784eecf01e40d
narumincho <narumincho.starfy@gmail.com>
2021-03-09 19:32:35 +0900

    feat: プロジェクト作成画面

--- client/page/NewProject.tsx
+++ client/page/NewProject.tsx
@@ -1,17 +1,67 @@
 import * as React from "react";
 import * as d from "../../data";
+import { Box, Button, TextField, makeStyles } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import { AppBar } from "../ui";
-import { Box } from "@material-ui/core";
+import { stringToValidProjectName } from "../../common/validation";
+
+const useStyles = makeStyles({
+  createButton: {
+    textTransform: "none",
+  },
+});
 
 export const NewProject: React.VFC<{
   accountToken: d.AccountToken;
   account: d.QAccount;
 }> = (props) => {
+  const classes = useStyles();
+  const [projectName, setProjectName] = React.useState<string>("");
+  const [isFirst, setIsFirst] = React.useState<boolean>(true);
+
+  const projectNameResult = stringToValidProjectName(projectName);
   return (
     <Box>
       <AppBar title="プロジェクト作成" account={props.account}></AppBar>
       <Box padding={1}>
-        プロジェクト作成に必要なフォームと送信ボタンを置きたい
+        <Box padding={1}>
+          <TextField
+            required
+            fullWidth
+            label="プロジェクト名"
+            value={projectName}
+            onChange={(e) => {
+              setProjectName(e.target.value);
+            }}
+            error={!isFirst && projectNameResult._ === "Error"}
+            helperText={
+              !isFirst && projectNameResult._ === "Error"
+                ? projectNameResult.error
+                : undefined
+            }
+            variant="outlined"
+          />
+        </Box>
+        <Box padding={1}>
+          <Button
+            fullWidth
+            onClick={() => {
+              setIsFirst(false);
+              console.log("作成ボタンを押された");
+            }}
+            size="large"
+            disabled={!isFirst && projectNameResult._ === "Error"}
+            variant="contained"
+            color="primary"
+            className={classes.createButton}
+            startIcon={<Add />}
+          >
+            {projectNameResult._ === "Ok"
+              ? `「${projectNameResult.ok}」`
+              : "プロジェクト"}
+            を作成
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
---
+++ common/validation.ts
@@ -0,0 +1,41 @@
+import * as d from "../data";
+
+/**
+ * プロジェクト名を正規化する.
+ */
+export const stringToValidProjectName = (
+  text: string
+): d.Result<string, string> => {
+  const normalized = normalizeOneLineString(text);
+  const { length } = [...normalized];
+  if (length <= 0) {
+    return d.Result.Error("プロジェクト名が空です");
+  }
+  if (length <= 0 || length > 100) {
+    return d.Result.Error("プロジェクト名は100文字以内です");
+  }
+  return d.Result.Ok(normalized);
+};
+
+/**
+ * NFKCで正規化して, 先頭末尾の空白をなくし, 空白の連続を1つの空白にまとめ, 改行を取り除く
+ */
+const normalizeOneLineString = (text: string): string => {
+  const normalized = text.normalize("NFKC").trim();
+  let result = "";
+  let beforeSpace = false;
+  for (const char of normalized) {
+    const codePoint = char.codePointAt(0);
+    // 制御文字
+    if (
+      codePoint !== undefined &&
+      ((codePoint > 0x1f && codePoint < 0x7f) || codePoint > 0xa0)
+    ) {
+      if (!(beforeSpace && char === " ")) {
+        result += char;
+        beforeSpace = char === " ";
+      }
+    }
+  }
+  return result;
+};
