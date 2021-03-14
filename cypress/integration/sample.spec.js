a5d7468493522bf212b8a1e42da3564e64ffd8b7
narumincho <narumincho.starfy@gmail.com>
2021-03-15 08:23:36 +0900

    feat: cypress での e2eテスト導入

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("My First Test", () => {
+  it("最初に訪れたとき, LINEでログインボタンが表示される", () => {
+    cy.visit("http://localhost:5000");
+
+    cy.contains("LINE で ログイン");
+  });
+});
