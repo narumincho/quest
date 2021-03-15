7b4bbbaebb4046676808030b2d558aec9935c49d
narumincho <narumincho.starfy@gmail.com>
2021-03-15 09:37:25 +0900

    feat: プログラム作成テスト追加

--- cypress/integration/noLogin.spec.ts
+++ cypress/integration/noLogin.spec.ts
@@ -1,4 +1,8 @@
-describe("My First Test", () => {
+beforeEach(() => {
+  indexedDB.deleteDatabase("main");
+});
+
+describe("ログイン前", () => {
   it("最初に訪れたとき, LINEでログインボタンが表示される", () => {
     cy.visit("http://localhost:5000");
 
