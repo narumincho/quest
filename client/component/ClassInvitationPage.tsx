f3b30c470ffc1d8cf1bd49f5706372a2477bdb59
narumincho <narumincho.starfy@gmail.com>
2021-05-15 11:18:20 +0900

    feat: クラスに参加するボタンの表示

--- client/component/ClassInvitationPage.tsx
+++ client/component/ClassInvitationPage.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as d from "../../data";
 import { AppState } from "../state";
+import { Button } from "@material-ui/core";
 import { PageContainer } from "./PageContainer";
 
 export const ClassInvitationPage: React.VFC<{
@@ -10,6 +11,7 @@ export const ClassInvitationPage: React.VFC<{
   return (
     <PageContainer isHideBack appState={props.appState}>
       招待URLを開いたようだね
+      <Button>[クラス名]に参加する</Button>
     </PageContainer>
   );
 };
