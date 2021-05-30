205df9f8923694167f4d18ded0d42165655b82ec
narumincho <narumincho.starfy@gmail.com>
2021-05-30 20:49:34 +0900

    fix: LogInPage width, height

--- client/component/LogInPage.tsx
+++ client/component/LogInPage.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { Box, Typography } from "@material-ui/core";
-import { AppBar } from "./AppBar";
 import { AppState } from "../state";
 import { LineLoginButton } from "./LineLoginButton";
+import { PageContainer } from "./PageContainer";
 
 export type Props = {
   readonly appState: AppState;
@@ -10,16 +10,15 @@ export type Props = {
 
 export const LogInPage: React.VFC<Props> = (props) => {
   return (
-    <Box>
-      <AppBar appState={props.appState} isHideBack />
+    <PageContainer appState={props.appState} isHideBack>
       <Box padding={1}>
         <Typography variant="body1">
           クエストを使うためにはLINEログインが必要です
         </Typography>
-        <Box padding={1}>
-          <LineLoginButton appState={props.appState} />
-        </Box>
       </Box>
-    </Box>
+      <Box padding={1}>
+        <LineLoginButton appState={props.appState} />
+      </Box>
+    </PageContainer>
   );
 };
