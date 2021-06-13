93b6629ff3160e180db8f477295d05035bf69eb8
narumincho <narumincho.starfy@gmail.com>
2021-06-13 12:39:49 +0900

    client/state/question の 不要なhookを削除

--- client/state/question.ts
+++ client/state/question.ts
@@ -1,99 +1,20 @@
 import * as d from "../../data";
 import { questionParentIsValid } from "../../common/validation";
-import { useState } from "react";
 
+/** 質問の木構造 */
 export type QuestionTree = {
-  id: d.QQuestionId;
-  text: string;
-  children: ReadonlyArray<QuestionTree>;
+  readonly id: d.QQuestionId;
+  readonly text: string;
+  readonly children: ReadonlyArray<QuestionTree>;
 };
 
 export type UseQuestionMapResult = {
-  /** 質問をすべて置き換える (storybook 用) */
-  readonly overwriteQuestionList: (
-    questionList: ReadonlyArray<d.QQuestion>
-  ) => void;
   /** 質問をリロードするまで保存する */
   readonly setQuestion: (question: d.QQuestion) => void;
   /** 複数の質問を一度にリロードするまで保存する */
   readonly setQuestionList: (questionList: ReadonlyArray<d.QQuestion>) => void;
   /** QuestionId から 質問を得る */
   readonly questionById: (id: d.QQuestionId) => d.QQuestion | undefined;
-  /** 質問の子を取得する */
-  readonly questionChildren: (
-    id: d.QQuestionId
-  ) => ReadonlyArray<d.QQuestionId>;
-  readonly getParentQuestionList: (
-    id: d.QQuestionId
-  ) => ReadonlyArray<d.QQuestion>;
-  readonly questionTree: (id: d.QProgramId) => ReadonlyArray<QuestionTree>;
-  /** ログアウトしたとき, キャッシュを削除する */
-  readonly deleteAll: () => void;
-  /** 親の質問になることができる質問を, キャッシュから取得する */
-  readonly getQuestionThatCanBeParentList: (
-    programId: d.QProgramId,
-    questionId: d.QQuestionId
-  ) => ReadonlyArray<d.QQuestion>;
-};
-
-export const useQuestionMap = (): UseQuestionMapResult => {
-  const [questionMap, setQuestionMap] = useState<
-    ReadonlyMap<d.QQuestionId, d.QQuestion>
-  >(new Map());
-  return {
-    /** 質問をすべて置き換える (storybook 用) */
-    overwriteQuestionList: (questionList: ReadonlyArray<d.QQuestion>): void => {
-      setQuestionMap(
-        new Map(
-          questionList.map((question) => [question.id, question] as const)
-        )
-      );
-    },
-    /** 質問をリロードするまで保存する */
-    setQuestion: (question: d.QQuestion): void => {
-      setQuestionMap((before) => {
-        return new Map(before).set(question.id, question);
-      });
-    },
-    /** 複数の質問を一度にリロードするまで保存する */
-    setQuestionList: (questionList: ReadonlyArray<d.QQuestion>): void => {
-      setQuestionMap((before) => {
-        const map = new Map(before);
-        for (const question of questionList) {
-          map.set(question.id, question);
-        }
-        return map;
-      });
-    },
-    /** QuestionId から 質問を得る */
-    questionById: (id: d.QQuestionId) => {
-      return questionMap.get(id);
-    },
-    questionChildren: (id: d.QQuestionId): ReadonlyArray<d.QQuestionId> =>
-      questionChildren(id, questionMap),
-    getParentQuestionList: (id: d.QQuestionId): ReadonlyArray<d.QQuestion> =>
-      getParentQuestionList(id, questionMap),
-    questionTree: (programId): ReadonlyArray<QuestionTree> =>
-      getQuestionTree(programId, [...questionMap.values()]),
-    deleteAll: () => {
-      setQuestionMap(new Map());
-    },
-    getQuestionThatCanBeParentList: (programId, questionId) => {
-      const result: Array<d.QQuestion> = [];
-      for (const question of questionMap.values()) {
-        const validationResult = questionParentIsValid(
-          question.id,
-          questionId,
-          programId,
-          questionMap
-        );
-        if (validationResult.isValid) {
-          result.push(question);
-        }
-      }
-      return result;
-    },
-  };
 };
 
 export const getQuestionThatCanBeParentList = (
@@ -132,6 +53,7 @@ export const questionChildren = (
   return result;
 };
 
+/** 親の質問になることができる質問を, キャッシュから取得する */
 export const getParentQuestionList = (
   id: d.QQuestionId,
   questionMap: ReadonlyMap<d.QQuestionId, d.QQuestion>
