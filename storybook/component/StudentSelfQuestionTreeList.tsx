a56a971901c6fe94b79a67505aa4f7ca58674eff
narumincho <narumincho.starfy@gmail.com>
2021-08-13 18:50:37 +0900

    fix type, pick mockStudentSelfQuestionTreeList

--- storybook/component/StudentSelfQuestionTreeList.tsx
+++ storybook/component/StudentSelfQuestionTreeList.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import * as d from "../../data";
 import { Meta, Story } from "@storybook/react";
 import { StudentSelfQuestionTreeList } from "../../client/component/StudentSelfQuestionTreeList";
+import { mockStudentSelfQuestionTreeList } from "../mock";
 
 const meta: Meta = {
   title: "StudentSelfQuestionTreeList",
@@ -10,60 +10,5 @@ const meta: Meta = {
 export default meta;
 
 export const Default: Story<never> = () => (
-  <StudentSelfQuestionTreeList
-    treeList={[
-      {
-        answer: d.Maybe.Nothing(),
-        questionId: d.QQuestionId.fromString("sampleA"),
-        questionText: "学生時代に頑張ったこと",
-        children: [
-          {
-            answer: d.Maybe.Nothing(),
-            questionText: "どんな課題に取り組んだか",
-            children: [],
-            questionId: d.QQuestionId.fromString("sampleAA"),
-          },
-          {
-            answer: d.Maybe.Nothing(),
-            questionText: "どんな目標に取り組んだか",
-            children: [],
-            questionId: d.QQuestionId.fromString("sampleAB"),
-          },
-          {
-            answer: d.Maybe.Nothing(),
-            questionText: "実際の取り組みはどのようなものだったのか",
-            children: [],
-            questionId: d.QQuestionId.fromString("sampleAC"),
-          },
-          {
-            answer: d.Maybe.Just({
-              isConfirm: false,
-              text: "良い結果を得られた. 具体的な内容は……",
-            }),
-            questionText: "結果はそのようなものが得られたか",
-            children: [],
-            questionId: d.QQuestionId.fromString("sampleAD"),
-          },
-          {
-            answer: d.Maybe.Just({
-              isConfirm: true,
-              text: "はい",
-            }),
-            questionText: "学びは得られたか?",
-            children: [],
-            questionId: d.QQuestionId.fromString("sampleAE"),
-          },
-        ],
-      },
-      {
-        answer: d.Maybe.Just({
-          isConfirm: true,
-          text: "私は人間です",
-        }),
-        questionId: d.QQuestionId.fromString("sampleB"),
-        questionText: "自己紹介",
-        children: [],
-      },
-    ]}
-  />
+  <StudentSelfQuestionTreeList treeList={mockStudentSelfQuestionTreeList} />
 );
--- storybook/mock.ts
+++ storybook/mock.ts
@@ -295,3 +295,59 @@ export const mockLoggedInState: LoggedInState = {
     ],
   ]),
 };
+
+export const mockStudentSelfQuestionTreeList: ReadonlyArray<d.StudentSelfQuestionTree> =
+  [
+    {
+      answer: d.Maybe.Nothing(),
+      questionId: d.QQuestionId.fromString("sampleA"),
+      questionText: "学生時代に頑張ったこと",
+      children: [
+        {
+          answer: d.Maybe.Nothing(),
+          questionText: "どんな課題に取り組んだか",
+          children: [],
+          questionId: d.QQuestionId.fromString("sampleAA"),
+        },
+        {
+          answer: d.Maybe.Nothing(),
+          questionText: "どんな目標に取り組んだか",
+          children: [],
+          questionId: d.QQuestionId.fromString("sampleAB"),
+        },
+        {
+          answer: d.Maybe.Nothing(),
+          questionText: "実際の取り組みはどのようなものだったのか",
+          children: [],
+          questionId: d.QQuestionId.fromString("sampleAC"),
+        },
+        {
+          answer: d.Maybe.Just({
+            isConfirm: false,
+            text: "良い結果を得られた. 具体的な内容は……",
+          }),
+          questionText: "結果はそのようなものが得られたか",
+          children: [],
+          questionId: d.QQuestionId.fromString("sampleAD"),
+        },
+        {
+          answer: d.Maybe.Just({
+            isConfirm: true,
+            text: "はい",
+          }),
+          questionText: "学びは得られたか?",
+          children: [],
+          questionId: d.QQuestionId.fromString("sampleAE"),
+        },
+      ],
+    },
+    {
+      answer: d.Maybe.Just({
+        isConfirm: true,
+        text: "私は人間です",
+      }),
+      questionId: d.QQuestionId.fromString("sampleB"),
+      questionText: "自己紹介",
+      children: [],
+    },
+  ];
